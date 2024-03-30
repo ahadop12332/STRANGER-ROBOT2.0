3f4360a002cb1081d0373ba2045e8bbabb8e73cd
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-03-30 07:57:41 +0530

    Update sexy.py

--- MukeshRobot/modules/sexy.py
+++ MukeshRobot/modules/sexy.py
@@ -4,7 +4,7 @@ from telethon import Button, events
 
 from .. import telethn as asst
 
-BUTTON = [[Button.url("❁❁ ꜱᴜᴘᴘᴏʀᴛ ❁❁", "https://t.me/mastiwithfriendsx")]]
+BUTTON = [[Button.url("❁❁ ꜱᴜᴘᴘᴏʀᴛ ❁❁", "https://t.me/MASTIWITHFRIENDSXD")]]
 HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
 SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
 LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
